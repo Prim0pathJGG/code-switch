5d2fa16d1f3eb29a5f1b15cb838f768a663d7e01
Johnathan <jgreengroves@gmail.com>
2026-03-21 11:17:37 -0400

    modified stars.js

--- js/stars.js
+++ js/stars.js
@@ -13,13 +13,13 @@
 
 //     document.body.appendChild(starsContainer);
 // });
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const numStars = 10;
   const starsContainer = document.createDocumentFragment();
 
   for (let i = 0; i < numStars; i++) {
-    const star = document.createElement("div");
-    star.classList.add("star");
+    const star = document.createElement('div');
+    star.classList.add('star');
     star.style.left = `${Math.random() * 100}vw`;
     star.style.animationDuration = `${Math.random() * 40 + 10}s`;
     star.style.animationDelay = `${Math.random() * 15}s`;
